7ed0d7cf0054a595c04548a5b08ee200a6a3602f
Henrique Mendes <henriquemendes13@gmail.com>
2023-09-18 15:01:21 -0300

    Add project files.

---
+++ NumerosImpare/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, 
+ * um valor por linha, inclusive o X, se for o caso.*/
+
+int x;
+
+x=int.Parse(Console.ReadLine());
+
+for(int i=1; i<=x; i=i+2)
+{
+    int impares = 0 + i;
+    Console.WriteLine(impares);
+}
